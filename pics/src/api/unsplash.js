e35dd09b87c7507a60d39359973400db78af65f5
ZakBrinlee <zbrinlee@hotmail.com>
2019-09-25 20:39:29 -0700

    Completed section 9 - Implementing Lists

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 // creating a custom instance of axios
+// unsplash was made with a test email account - client key is not needing to be secure
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
